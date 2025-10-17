9810ff10b9195c3ce934d18fe5a4089336ff90c7
Sohel Shaikh <sohel.shaikh20753@gmail.com>
2025-10-17 19:56:48 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,22 @@
-// import React from "react";
-// import logo from "../assets/logo.png";
-
-// function Navbar() {
-//   return (
-//     <div className="navbar">
-//       <img src={logo} alt="Logo" style={{ height: "40px", marginRight: "10px" }} />
-//       React News App
-//     </div>
-//   );
-// }
-
-// export default Navbar;
-
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
-function Navbar({ onCategoryChange }) {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const categories = [
-    "General",
-    "Business",
-    "Technology",
-    "Sports",
-    "Entertainment",
-    "Health",
-    "Science",
-  ];
-
-  const handleCategoryClick = (category) => {
-    onCategoryChange(category);
-    setIsMobileMenuOpen(false); // close mobile menu on click
-  };
-
+function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <img src={logo} alt="Logo" className="navbar-logo" />
-        <span className="navbar-title">Infowave News</span>
+      <div className="nav-left">
+        <img src={logo} alt="logo" className="logo" />
+        <span className="brand">Infowave News</span>
       </div>
-
-      <div className={`navbar-links ${isMobileMenuOpen ? "active" : ""}`}>
-        {categories.map((cat) => (
-          <button
-            key={cat}
-            className="navbar-link"
-            onClick={() => handleCategoryClick(cat)}
-          >
-            {cat}
-          </button>
-        ))}
-      </div>
-
-      <div
-        className="navbar-toggle"
-        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-      >
-        &#9776; {/* hamburger icon */}
+      <div className="nav-links">
+        <Link to="/">General</Link>
+        <Link to="/business">Business</Link>
+        <Link to="/technology">Technology</Link>
+        <Link to="/sports">Sports</Link>
+        <Link to="/entertainment">Entertainment</Link>
+        <Link to="/health">Health</Link>
+        <Link to="/science">Science</Link>
       </div>
     </nav>
   );
